3d6dcdce3d29710e1e181439e7d6161a6853e3d3
Eshita <edas25564@gmail.com>
2023-02-15 12:44:35 +0530

    "three types of mobile donee"

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const slideIndex=0;
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("quote");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
+}
