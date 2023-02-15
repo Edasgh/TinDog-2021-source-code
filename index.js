de8fcecec03ad70c65a25c1aea4e7d7de13b661d
Eshita <edas25564@gmail.com>
2023-02-15 14:48:41 +0530

    "Button functionality of carousel-section"

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 7000); // Change image every 7 seconds
+
 }
 
 let listBtn=document.querySelector('.fa-list');
@@ -20,4 +21,16 @@ let nav=document.querySelector('.navbar');
 
 listBtn.addEventListener('click',()=>{
  nav.classList.toggle('d-none');
+})
+
+  
+let nxtBtn=document.querySelector('.next-btn');
+let prevBtn=document.querySelector('.prev-btn');
+nxtBtn.addEventListener('click',()=>{
+ showSlides();
+})
+
+prevBtn.addEventListener('click',()=>{
+ showSlides();
+  
 })
